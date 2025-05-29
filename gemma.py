72dc228ee04c99cbf7c8a288e813c3e75529af70
JeffSSampaio <jefferson.sampaio@cest.edu.br>
2025-05-29 11:56:00 -0300

    test(modelo): implementando timing para avaliar performance do gemma-2-9b

---
+++ gemma.py
@@ -0,0 +1,16 @@
+import lmstudio as lms
+import time as time
+
+modelo = lms.llm("google/gemma-2-9b")
+
+prompt = input("você:")
+
+inicio = time.time()
+
+resposta = modelo.respond(prompt)
+
+fim = time.time()
+
+tempoResposta = fim - inicio
+
+print(f"Modelo gemma-2-9b\nPrompt: {prompt}\nResposta do modelo:{resposta}\nTempo de Resposta: {tempoResposta:.2f} segundos")
